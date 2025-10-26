39ad0bdf7071bd8402758d950967f5062d7f3f6e
= <=>
2025-10-27 02:54:47 +0300

    added pydantic validation. 12:27

---
+++ src/helpers/config.py
@@ -0,0 +1,22 @@
+import os
+import logging
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+class Settings(BaseSettings):
+
+    APP_NAME: str
+    APP_VERSION: str
+    OPENAI_API_KEY: str
+
+    class Config:
+        # check the file exist test before running
+        envfile = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ".env")
+        print("Using envfile at:", envfile)
+
+
+def get_settings():
+    return Settings()
--- src/main.py
+++ src/main.py
@@ -1,8 +1,4 @@
 from fastapi import FastAPI
-
-from dotenv import load_dotenv
-load_dotenv(dotenv_path= ".env")  # Load environment variables from .env file
-
 from routes import base
 
 app = FastAPI()
